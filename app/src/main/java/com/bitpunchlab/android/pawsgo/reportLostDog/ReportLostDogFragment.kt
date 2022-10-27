261151211dbcff18a82ec50a980a3943a507f7b4
Jess on the Hope <jesshon.py326@gmail.com>
2022-10-27 18:14:10 -0400

    finished date picker and time picker and the corresponding functions
    implementing request location permissions.  show an alert if permissions were not granted.

--- app/src/main/java/com/bitpunchlab/android/pawsgo/reportLostDog/ReportLostDogFragment.kt
+++ app/src/main/java/com/bitpunchlab/android/pawsgo/reportLostDog/ReportLostDogFragment.kt
@@ -1,5 +1,9 @@
 package com.bitpunchlab.android.pawsgo.reportLostDog
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,6 +12,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
 import android.widget.TimePicker
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import com.bitpunchlab.android.pawsgo.R
 import com.bitpunchlab.android.pawsgo.databinding.FragmentReportLostDogBinding
@@ -29,6 +36,7 @@ class ReportLostDogFragment : Fragment() {
     private var lostDate : String? = null
     private var lostHour : Int? = null
     private var lostMinute : Int? = null
+    private var allPermissionGranted = MutableLiveData<Boolean>(true)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,6 +59,16 @@ class ReportLostDogFragment : Fragment() {
         binding.buttonChooseTime.setOnClickListener {
             showTimePicker()
         }
+
+        if (!checkPermission()) {
+            permissionResultLauncher.launch(permissions)
+        }
+
+        allPermissionGranted.observe(viewLifecycleOwner, androidx.lifecycle.Observer { value ->
+            if (!value) {
+                permissionsRequiredAlert()
+            }
+        })
 
         return binding.root
     }
@@ -108,6 +126,74 @@ class ReportLostDogFragment : Fragment() {
             lostHour = timePicker!!.hour
             lostMinute = timePicker!!.minute
             Log.i("got time back", "hour: ${lostHour.toString()} minute: ${lostMinute.toString()}")
+        }
+    }
+
+    private var permissions =
+        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            )
+
+    private val permissionResultLauncher =
+        registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+            //allPermissionGranted = true
+            permissions.entries.forEach {
+                if (!it.value) {
+                    Log.e("result launcher", "Permission ${it.key} not granted : ${it.value}")
+                    allPermissionGranted.value = false
+                }
+            }
+            /*
+            showDialog(
+                "Required",
+                "App needs Bluetooth and Location permissions to scan bluetooth devices.",
+                "Later",
+                "Allow",
+                object : DialogView.ButtonListener {
+                    override fun onNegativeButtonClick(dialog: AlertDialog) {
+                        dialog.dismiss()
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.fromParts("package", requireActivity().applicationContext.packageName, null))
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                    }
+
+                    override fun onPositiveButtonClick(dialog: AlertDialog) {
+                        dialog.dismiss()
+                        requestRequirdPermissions()
+                    }
+                })
+
+             */
+        }
+
+    private fun checkPermission() : Boolean {
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(requireContext(), permission) !=
+                PackageManager.PERMISSION_GRANTED) return false
+        }
+        return true
+    }
+
+    private fun permissionsRequiredAlert() {
+        val permissionAlert = AlertDialog.Builder(context)
+
+        with(permissionAlert) {
+            setTitle(getString(R.string.location_permissions_alert))
+            setMessage(getString(R.string.location_permissions_alert_desc))
+            setPositiveButton(getString(R.string.ok),
+                DialogInterface.OnClickListener { dialog, button ->
+                    dialog.dismiss()
+                    //permissionResultLauncher.launch(permissions)
+
+                })
+            //setNegativeButton(getString(R.string.cancel),
+            //    DialogInterface.OnClickListener { dialog, button ->
+            //        dialog.dismiss()
+            //    })
+            show()
         }
     }
 }
